c1d0984dc94520ea8ade00a6716d25083b8da835
Kate <braumwinter@gmail.com>
2020-03-19 12:09:01 +0300

    feat: add solve dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,23 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  if (members.length === 0) {
+    throw Error;
+  }
+
+  let arr = [];
+
+  for (let index in members) {
+    if (typeof (members[index]) === 'string') {
+      let temp = members[index].trim();
+      arr.push(temp[0].toLocaleUpperCase());
+    }
+  }
+
+  arr.sort();
+  let str = arr.join('');
+
+  return str;
 };
