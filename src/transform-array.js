64f8ea96e0d058078359234eb4d103d7ce6aed30
Kate <braumwinter@gmail.com>
2020-03-19 17:12:34 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,37 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    toString = {}.toString;
+    if (toString.call(arr) != "[object Array]") {
+        throw Error;
+    }
+
+    let flagsWork = false;
+
+    if (arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-next') || arr.includes('--double-prev')) {
+        flagsWork = true;
+    }
+
+    if (!flagsWork) {
+        return arr;
+    }
+
+    let rezult = [];
+
+    for (let index = 0; index < arr.length; index++) {
+        if ((arr[index] == '--double-prev') && (index != 0)) {
+            rezult.push(arr[index - 1]);
+        } else if ((arr[index] == '--double-next') && (index != arr.length - 1)) {
+            rezult.push(arr[index + 1]);
+        } else if ((arr[index] == '--discard-prev') && (index != 0)) {
+            rezult.pop();
+        }
+         else if(arr[index] != '--double-prev' && arr[index] != '--double-next' && arr[index] != '--discard-prev' && arr[index] != '--discard-next'){
+            rezult.push(arr[index]);
+
+            if((arr[index - 1] == '--discard-next') && (index != arr.length - 1)) {
+                rezult.pop();
+            }
+        }
+    }
+
+    return rezult;
 };
